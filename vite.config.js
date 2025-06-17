b8bf3a60040364803d6d6c5e05cd00089740f9fa
High-Devinition <High-Devinition@outlook.com>
2025-06-17 16:47:24 -0500

    Set Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  }
-})
+  base: '/swgoh-essentials/', // <-- Add this line
+});
